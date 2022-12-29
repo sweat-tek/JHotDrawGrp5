0a109d425f2e6a9cbaa771a5632cd3a25f1bcc76
Anders Lindhardt Madsen <andem20@student.sdu.dk>
2022-12-29 19:22:50 +0100

    init commit

---
+++ jhotdraw-samples/jhotdraw-samples-misc/src/test/java/MainTest.java
@@ -0,0 +1,12 @@
+import org.jhotdraw.samples.svg.Main;
+import org.junit.Test;
+
+public class MainTest {
+    @Test
+    public void testMain() {
+        String[] args = {};
+        Main.main(args);
+        while (true){
+        }
+    }
+}
