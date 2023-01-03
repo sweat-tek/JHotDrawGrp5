f1662ee3e65ccaf3f2c8e183ae7acddd37383645
Anders Lindhardt Madsen <andem20@student.sdu.dk>
2023-01-03 09:49:46 +0100

    add SVGImageFigure test

---
+++ jhotdraw-samples/jhotdraw-samples-misc/src/test/java/org/jhotdraw/samples/svg/figures/SVGImageFigureTest.java
@@ -0,0 +1,67 @@
+package org.jhotdraw.samples.svg.figures;
+
+import junit.framework.AssertionFailedError;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.junit.Assert.*;
+
+public class SVGImageFigureTest {
+    private final String IMAGE_PATH = "src/test/resources/org.jhotdraw/";
+
+    @Test
+    public void loadImageFromFileTest() {
+        SVGImageFigure svgImageFigure = new SVGImageFigure();
+        URI uri = URI.create("file:" + Paths.get(IMAGE_PATH + "actions.png").toAbsolutePath());
+        File file = new File(uri);
+
+        try {
+            svgImageFigure.loadImage(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new AssertionFailedError();
+        }
+
+        assertThrows(IOException.class, () -> {
+            URI newUri = URI.create("file:" + Paths.get(IMAGE_PATH + "corruptFile.png").toAbsolutePath());
+            File corruptFile = new File(newUri);
+            svgImageFigure.loadImage(corruptFile);
+        });
+    }
+
+    @Test
+    public void loadImageFromInputStreamTest() {
+        SVGImageFigure svgImageFigure = new SVGImageFigure();
+        URI uri = URI.create("file:" + Paths.get(IMAGE_PATH + "actions.png").toAbsolutePath());
+
+        InputStream file;
+        try {
+            file = Files.newInputStream(new File(uri).toPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        assertNotNull(file);
+
+        try {
+            svgImageFigure.loadImage(file);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new AssertionFailedError();
+        }
+
+        String message = assertThrows(IOException.class, () -> {
+            URI newUri = URI.create("file:" + Paths.get(IMAGE_PATH + "corruptFile.png").toAbsolutePath());
+            InputStream corruptInputStream = Files.newInputStream(new File(newUri).toPath());
+            svgImageFigure.loadImage(corruptInputStream);
+        }).getMessage();
+
+        assertTrue(message.contains("Couldn't load image from file"));
+    }
+}
